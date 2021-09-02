456af96473b5ed3b81f0befc591bdc7e4ccf12e2
SumatPurewal <56806773+SumatPurewal@users.noreply.github.com>
2021-09-02 21:37:53 +0530

    Ignore video switch failure for testing

--- drivers/media/platform/xilinx/xilinx-axis-switch.c
+++ drivers/media/platform/xilinx/xilinx-axis-switch.c
@@ -423,11 +423,11 @@ static int xvsw_parse_of(struct xvswitch_device *xvsw)
 		xvsw->saxi_ctlclk = devm_clk_get(xvsw->dev,
 						 "s_axi_ctrl_clk");
 		if (IS_ERR(xvsw->saxi_ctlclk)) {
-			ret = PTR_ERR(xvsw->saxi_ctlclk);
+			/*ret = PTR_ERR(xvsw->saxi_ctlclk);
 			dev_err(xvsw->dev,
 				"failed to get s_axi_ctrl_clk (%d)\n",
 				ret);
-			return ret;
+			return ret;*/
 		}
 	}
 
